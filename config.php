77da7a603f414437530ae5c41cd83d3f4348ac2d
bolidewang <bolidewang@users.noreply.github.com>
2023-02-10 14:42:31 +0800

    Update config.php
    keycloak issuer url

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
-$config->oidc->issuer="https://sts.qianqiusoft.com";
+$config->oidc->issuer="https://127.0.0.1:8443/auth/realms/sso-test";
 $config->oidc->clientId="local_test";
 $config->oidc->clientSecret="local_secret";
-
-
 
 ?>
